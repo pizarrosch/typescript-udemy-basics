8fdd01c22ad3e5a02b0ef97cdbe48769401d0604
pizarrosch <bayernsch89@gmail.com>
2023-03-19 17:24:58 +0100

    Class inheritance (private and protected properties)

--- compiledJS/classes.js
+++ compiledJS/classes.js
@@ -1,10 +1,13 @@
 "use strict";
 class Department {
+    // inherited classes
+    // "protected" restricts the change of the property from the outside as well,
+    // but, unlike "private", allows to change it from inherited classes
     constructor(id, name) {
         this.id = id;
         this.name = name;
-        // name: string --> there is another better method of setting the variables (see in the constructor brackets)
-        this.employees = [];
+        // name: string --> there is another better method of setting the properties (see in the constructor brackets)
+        this.employees = []; //"private" restricts the change of the property from the outside, as well as from
         // this.name = name;
     }
     describe() {
@@ -20,6 +23,34 @@ class Department {
         console.log(this.employees);
     }
 }
-
-const sales = new Department(1, 'Sales');
-sales.render();
+class IT extends Department {
+    constructor(id, admins) {
+        super(id, 'IT');
+        this.admins = admins;
+        this.admins = admins;
+    }
+    addEmployee(name) {
+        this.employees.push(name);
+    }
+}
+class reportsDepartment extends Department {
+    constructor(id, reports) {
+        super(id, 'reportsDepartment');
+        this.reports = reports;
+        this.reports = reports;
+    }
+    createReports(text) {
+        this.reports.push(text);
+    }
+}
+const dept = new Department(1, 'Department');
+const it = new IT(2, ['Bean', 'Jackson']);
+const reports = new reportsDepartment(3, []);
+dept.render();
+console.log(dept);
+it.addEmployee('Stanley');
+it.render();
+console.log(it);
+reports.render();
+reports.createReports('Yesterday new devices were purchased');
+console.log(reports);
--- src/classes.ts
+++ src/classes.ts
@@ -1,6 +1,9 @@
 class Department {
-    // name: string --> there is another better method of setting the variables (see in the constructor brackets)
-    private employees: string[] = []
+    // name: string --> there is another better method of setting the properties (see in the constructor brackets)
+    protected employees: string[] = [] //"private" restricts the change of the property from the outside, as well as from
+                                    // inherited classes
+                                    // "protected" restricts the change of the property from the outside as well,
+                                    // but, unlike "private", allows to change it from inherited classes
 
     constructor(private readonly id: number, private name: string) {
         // this.name = name;
@@ -23,10 +26,35 @@ class Department {
 }
 
 class IT extends Department {
-    constructor(id: number) {
+    constructor(id: number, public admins: string[]) {
         super(id, 'IT');
+        this.admins = admins;
+    }
+
+    addEmployee(name: string) {
+        this.employees.push(name);
     }
 }
 
-const sales = new Department(1, 'Sales');
-sales.render();
+class reportsDepartment extends Department {
+    constructor(id: number, public reports: string[]) {
+        super(id, 'reportsDepartment');
+        this.reports = reports;
+    }
+
+    createReports(text:string) {
+        this.reports.push(text);
+    }
+}
+
+const dept = new Department(1, 'Department')
+const it = new IT(2, ['Bean', 'Jackson']);
+const reports = new reportsDepartment(3, []);
+dept.render();
+console.log(dept);
+it.addEmployee('Stanley');
+it.render();
+console.log(it)
+reports.render();
+reports.createReports('Yesterday new devices were purchased')
+console.log(reports);
