77021be89e6805529ebfd3da18b3e6da3d4c1a83
pizarrosch <bayernsch89@gmail.com>
2023-03-26 15:31:24 +0200

    Use an interface for implementation

--- compiledJS/interface.js
+++ compiledJS/interface.js
@@ -1,36 +1,28 @@
-interface Person {
-    name: string;
-    age: number;
-    greet(text: string): void;
-}
-
+"use strict";
 // interfaces are used to define an object structure. The new object HAS to have all properties given in the interface;
-    //moreover, in the new object no extra properties can be created, which doesn't exist in the interface.
-
+//moreover, in the new object no extra properties can be created, which doesn't exist in the interface.
 // interfaces can also be used for classes, which implement the structure given in an interface. In this case additional
 // properties can be created in the class. This looks then like in an example below
-
-class Greeting implements Person {
-    name = 'Zaur';
-    age = 33;
-    greet(text: string) {
-        console.log(text)
+class Greeting {
+    constructor() {
+        this.name = 'Zaur';
+        this.age = 33;
     }
-
+    greet(text) {
+        console.log(text);
+    }
     render() {
         this.greet('Hello');
     }
 }
-
-let user1: Person;
-
+let user1;
 user1 = {
     name: 'Zaur',
     age: 33,
     greet(text) {
-    console.log(text)}
-}
-
-user1.greet(`Hello ${user1.name}! We congratulate you on your ${user1.age}d birthday!`)
+        console.log(text);
+    }
+};
+user1.greet(`Hello ${user1.name}! We congratulate you on your ${user1.age}d birthday!`);
 let greetings = new Greeting();
 greetings.render();
